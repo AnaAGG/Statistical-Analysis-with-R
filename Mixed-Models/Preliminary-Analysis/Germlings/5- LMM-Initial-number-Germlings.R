51936a694cb4283fd5a75b85786807847c300df7
Ana García <75084991+AnaAGG@users.noreply.github.com>
2021-07-02 14:43:57 +0200

    Preliminary analysis

--- Mixed-Models/Preliminary-Analysis/Germlings/5- LMM-Initial-number-Germlings.R
+++ Mixed-Models/Preliminary-Analysis/Germlings/5- LMM-Initial-number-Germlings.R
@@ -31,7 +31,7 @@ library(blmeco)
 Lenght<- read.table("clipboard", header=TRUE, sep = "\t")
 names(Lenght)
 
-
+# to categorise the variables
 Lenght$Disc<- as.factor(Lenght$Disc)
 Lenght$Salinity <- as.factor(Lenght$Salinity)
 Lenght$Temperature <- as.factor(Lenght$Temperature)
@@ -44,20 +44,21 @@ str(Lenght)
 options(contrasts=c(factor="contr.sum", ordered="contr.poly")) # para obtener los mismos resultados que STATISTICA y SPSS utilizando type III SS.
 mi.control <- lmerControl(check.conv.grad=.makeCC(action ="ignore", tol=1e-6, relTol=NULL), optimizer="bobyqa", optCtrl=list(maxfun=100000))
 
+# create all the possible models with all the possible combinations
 m0 <- lm (Initial ~ Population * Emersion , data=Lenght)
 m1 <- lmer(Initial ~ Population * Emersion  + (1|Tank), control=mi.control, data=Lenght)
 m2 <- lmer(Initial ~ Population * Emersion  + (Population|Tank), control=mi.control, data=Lenght)
 
 AICc(m0, m1, m2)
 
-#Exploraci�n de la normalidad (residuos)
+#Check the normality (residuals)
 hist(residuals(m2), density=5, freq=FALSE, main="residuos del modelo", ylim=c(0,0.004))
 curve(dnorm(x, mean=mean(residuals(m2)), sd=sd(residuals(m1))), col="red", lwd=2, add=TRUE, yaxt="n")
 qqnorm(residuals(m2), main="residuos del modelo")
 qqline(residuals(m2), col="red", lwd=2)
-shapiro.test(residuals(m2))#Hay mucha falta de normalidad y se ven algunos outliers bien definidos, lo que hacemos es quitarlos y volver a correr los modelos. 
+shapiro.test(residuals(m2)) # There is a lot of non-normality and you see some well-defined outliers, so what we do is remove them and re-run the models. 
 
-#modelos sin outliers
+# MODEL WITHOUT OUTLIERS
 Lenght2<- read.table("clipboard", header=TRUE, sep = "\t")
 m3 <- lm (Initial ~ Population * Emersion , data=Lenght2)
 m4 <- lmer(Initial ~ Population * Emersion  + (1|Tank), control=mi.control, data=Lenght2)
@@ -69,40 +70,38 @@ hist(residuals(m5), density=5, freq=FALSE, main="residuos del modelo", ylim=c(0,
 curve(dnorm(x, mean=mean(residuals(m5)), sd=sd(residuals(m5))), col="red", lwd=2, add=TRUE, yaxt="n")
 qqnorm(residuals(m5), main="residuos del modelo")
 qqline(residuals(m5), col="red", lwd=2)
-shapiro.test(residuals(m5))
+shapiro.test(residuals(m5)) # I still have problems of normality, so I ttransform the response variable to log
 
-#Transformamos la variable respuesta:
+# Transform the response variable
 
 m6 <- lmer (log(Initial) ~ Population * Emersion + (Population|Tank), control=mi.control, data=Lenght2)
 
-#Exploraci�n de la normalidad (residuos)
+# Check the normality (residuals)
 hist(residuals(m6), density=5, freq=FALSE, main="residuos del modelo", ylim=c(0,0.004))
 curve(dnorm(x, mean=mean(residuals(m6)), sd=sd(residuals(m2))), col="red", lwd=2, add=TRUE, yaxt="n")
 qqnorm(residuals(m6), main="residuos del modelo")
 qqline(residuals(m6), col="red", lwd=2)
-shapiro.test(residuals(m6))#No se ha corregido del todo, pero lo dejamos asi, esta cerca de 0.05
+shapiro.test(residuals(m6)) #It has not been fully corrected, but we leave it as it is, it is close to 0.05.
 
 
-#Exploraci�n de la heterocedasticidad (residuos)
-plot(fitted(m6), residuals(m6), main="�HAY HETEROCEDASTICIDAD?")#con los mixtos no hay las correcciones que hay en los generales/generalizados.
+# Check the heterocedasticity (residuals)
+plot(fitted(m6), residuals(m6), main="�HAY HETEROCEDASTICIDAD?")# with the mixed ones there are not the corrections that there are in the general/generalised ones.
 abline(h=0, col="red")
-leveneTest(residuals(m6) ~ Population * Emersion, data=Lenght2)#Homogeneidad de varianzas OK.
+leveneTest(residuals(m6) ~ Population * Emersion, data=Lenght2)# Variance homogeneity OK
 
 
-#Reducimos el modelo: 
-
-#Reducimos modelo:
+# Reduce the model 
 m7 <- update(m6, method="ML")
-m7 <- update(m6, REML = FALSE)#Ajustamos el modelo con ML porque luego las comparaciones de modelos van a ser con lrtest.
+m7 <- update(m6, REML = FALSE)# We adjust the model with ML because then the model comparisons are going to be with Irtest.
 m8 <- update(m6, ~. -Population : Emersion)
 summary(m8)
 Anova(m8, type = 3, test="Chisq")
-lrtest(m7,m8) # Sale significativo, por lo que no podemos seguir reduciendo el modelo. 
+lrtest(m7,m8) # It comes out significant, so we cannot reduce the model any further. 
 
 summary(m7)
 Anova(m7, type = 3, test="Chisq")
 r.squaredGLMM(m7)
-#Comparaciones posthoc, de los factores que salen significativos
+# Post-hoc comparisons, of the factors found to be significant
 
 require(lsmeans)
 lsm <- lsmeans(m7, pairwise ~ Population:Emersion, adjust = "Tukey")
