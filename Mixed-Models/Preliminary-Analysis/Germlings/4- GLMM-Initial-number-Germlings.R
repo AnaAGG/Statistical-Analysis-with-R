df215dd6d3a38cebbd7af52272a182d5fb56b8df
Ana García <75084991+AnaAGG@users.noreply.github.com>
2021-07-02 14:30:38 +0200

    Preliminary analysis

--- Mixed-Models/Preliminary-Analysis/Germlings/4- GLMM-Initial-number-Germlings.R
+++ Mixed-Models/Preliminary-Analysis/Germlings/4- GLMM-Initial-number-Germlings.R
@@ -1,3 +1,7 @@
+
+# CHECK IF THE INITIAL NUMBER OF GERMLINGS DEPENDS ON THE POPULATION. 
+# If yes, we have need to take this into account when interpreting the results
+
 library(lme4)     ## generalized mixed models
 library(lmerTest) ## para MS, df, p ... usando type 3/type 1 hypotheses with "Satterthwaite" and "Kenward-Roger"
 library(pbkrtest) ## necesario para lmerTest
@@ -12,54 +16,59 @@ library(LMERConvenienceFunctions) ## para tests post hoc
 library(blmeco)   ## para AICweights
 library(MASS)
 library(nlme)
+library(emmeans)  ## to post-hoc analysis
 
-##Para las poblaciones de rias##
+# --------------------- # --------------------- # --------------------- # --------------------- # --------------------- # --------------------- #
+# --------------------- # --------------------- # --------------------- # --------------------- # --------------------- # --------------------- #
+# TO RIA POPULATIONS
+
 numger<- read.table("clipboard", header = TRUE, sep="\t")
 
+# to categorise the variables
 numger$Tank<- as.factor(numger$Tank)
 numger$Salinity<- as.factor(numger$Salinity)
 numger$Population<- as.factor(numger$Population)
 numger$Temperature<- as.factor(numger$Temperature)
-str(numger)
+str(numger) # get the type of the objects (as dtypes in python)
 
 mi.control <- glmerControl(check.conv.grad=.makeCC(action ="ignore", tol=1e-6, relTol=NULL), optimizer="bobyqa", optCtrl=list(maxfun=100000))
 options(contrasts=c(factor="contr.sum", ordered="contr.poly"))
 
-##Al hacer promedios de los conteos de los cuadrados, disco no se puede meter como factor ya 
-##que solo tiene una medida.
+##When averaging the counts of the squares, disco cannot be factored in as it only has one measure. 
+##it only has one measure.
 
 
 m1 <- glm(Initial ~ Population , family=poisson(link="log"), data=numger)
 m2 <- glmer(Initial ~ Population + (1|Tank), family=poisson(link="log"), data=numger, control=mi.control)
-m3 <- glmer(Initial ~ Population + (Population|Tank), family=poisson(link="log"), data=numger, control=mi.control) # No se incluye este modelo por problemas de convergencia
-#Los tres modelos están ajustados con el método de ML por lo que son comparables con AICc.
-
+m3 <- glmer(Initial ~ Population + (Population|Tank), family=poisson(link="log"), data=numger, control=mi.control) # I dont include this model due to convergence problems
+# All three models are fitted with the ML method, thus are comparable with AICc
 anova(m2,m1)
 
 
 
-# pesos de los modelos comparados
+# model weights
 Weights(AICc(m1,m2))
-## ¿cuántas veces es mejor un modelo que otro?
-## las comparaciones relevantes podrían ser, colocando primero el modelo con menor AICc:
+
+## how many times is one model better than the other?
+## relevant comparisons could be, placing the model with the lowest AICc first:
 exp(-0.5*(AICc(m2)-AICc(m1)))
 
 dispersion_glmer(m2)
 summary(m2)
 Anova(m2, type=3, test="Chisq")
 
-#Otra forma de saber si existe sobredispersión, definida por el ratio entre la devianza 
-#residual y los grados de libertad residuales, si es mayor que 1 hay sobredispersión:
+#Another way to know if there is overdispersion, defined by the ratio between the residual deviancy and the residual degrees of freedom, if it is greater than 1 there is overdispersion. 
+#residual degrees of freedom, if it is greater than 1 there is overdispersion:
 rdev <- sum(residuals(m2)^2)
 mdf <- length(fixef(m2))
 rdf <- nrow(numger)-mdf
-rdev/rdf #Si es mayor que 1 hay sobredispersión
+rdev/rdf #if > 1 ==> OVERDISPERSION
 
-#Una solución para abordar el tema de la sobredispersión es crear una variable aleatoria
-#a nivel individual:
+#One solution to address the issue of overdispersion is to create a random variable.
+#at the individual level:
 numger$obs <- 1:nrow(numger) #create individual level random variable
 modelBQ<-glmer(Initial ~ Population + (Population|Tank) + (1|obs), data=numger, family=poisson(link="log"), control=mi.control)
-#Se analiza otra vez la dispersión:
+#I check the overdispersion again
 rdev <- sum(residuals(modelBQ)^2)
 mdf <- length(fixef(modelBQ))
 rdf <- nrow(numger)-mdf
@@ -68,21 +77,22 @@ dispersion_glmer(modelBQ)
 
 
 
-#Reducimos el modelo:
-#Selección de la parte fija:
+#Reduce the model:
+#Select the fixed part:
 
 options(na.action = "na.fail")
-sg<- dredge(modelBQ, rank="AICc", fixed = ~Population|Tank) #Evaluamos todos los posibles modelos, con todas las posibles combinaciones de factores fijos, fijando el factor aleatorio. 
+sg<- dredge(modelBQ, rank="AICc", fixed = ~Population|Tank) # Evaluating all the possible models, with all the possible combination, fixing the random part
 subset(sg, delta < 2, recalc.weights = TRUE)
 importance(subset(model.sel(sg), delta <= 2))
-
 Muminresults<- write.csv(sg, "MuMIn Preliminary results SurvivalGermlings_(A-M).csv")
 
 
 
-##Para todas las poblaciones##
-numger<- read.table("clipboard", header = TRUE, sep="\t")
+# --------------------- # --------------------- # --------------------- # --------------------- # --------------------- # --------------------- #
+# --------------------- # --------------------- # --------------------- # --------------------- # --------------------- # --------------------- #
+# ALL POPULATIONS
 
+numger<- read.table("clipboard", header = TRUE, sep="\t")
 numger$Tank<- as.factor(numger$Tank)
 numger$Salinity<- as.factor(numger$Salinity)
 numger$Temperature<- as.factor(numger$Temperature)
@@ -91,41 +101,38 @@ str(numger)
 mi.control <- glmerControl(check.conv.grad=.makeCC(action ="ignore", tol=1e-6, relTol=NULL), optimizer="bobyqa", optCtrl=list(maxfun=100000))
 options(contrasts=c(factor="contr.sum", ordered="contr.poly"))
 
-##Al hacer promedios de los conteos de los cuadrados, disco no se puede meter como factor ya 
-##que solo tiene una medida.
-
-
 m1 <- glm(Initial ~ Population * Emersion, family=poisson(link="log"), data=numger)
 m2 <- glmer(Initial ~ Population * Emersion + (1|Tank), family=poisson(link="log"), data=numger, control=mi.control)
 m3 <- glmer(Initial ~ Population * Emersion + (Population|Tank), family=poisson(link="log"), data=numger, control=mi.control)
-#Los tres modelos están ajustados con el método de ML por lo que son comparables con AICc.
 
 anova(m3,m2,m1)
 AICc(m1,m2,m3)
 
 
-# pesos de los modelos comparados
+# Model weights
 Weights(AICc(m1,m2,m3))
-## ¿cuántas veces es mejor un modelo que otro?
-## las comparaciones relevantes podrían ser, colocando primero el modelo con menor AICc:
+
+## how many times is one model better than the other?
+## relevant comparisons could be, placing the model with the lowest AICc first:
 exp(-0.5*(AICc(m3)-AICc(m2)))
 
 dispersion_glmer(m2)
 summary(m2)
 Anova(m2, type=3, test="Chisq")
 
-#Otra forma de saber si existe sobredispersión, definida por el ratio entre la devianza 
-#residual y los grados de libertad residuales, si es mayor que 1 hay sobredispersión:
+#Another way to know if there is overdispersion, defined by the ratio between the residual deviancy and the residual degrees of freedom, if it is greater than 1 there is overdispersion. 
+#residual degrees of freedom, if it is greater than 1 there is overdispersion:
 rdev <- sum(residuals(m2)^2)
 mdf <- length(fixef(m2))
 rdf <- nrow(numger)-mdf
-rdev/rdf #Si es mayor que 1 hay sobredispersión
+rdev/rdf #Si es mayor que 1 hay sobredispersiï¿½n
 
-#Una solución para abordar el tema de la sobredispersión es crear una variable aleatoria
-#a nivel individual:
+#One solution to address the issue of overdispersion is to create a random variable.
+#at the individual level:
 numger$obs <- 1:nrow(numger) #create individual level random variable
 modelBQ<-glmer(Initial ~ Population * Emersion  + (Population|Tank) + (1|obs), data=numger, family=poisson(link="log"), control=mi.control)
-#Se analiza otra vez la dispersión:
+
+#Check the overdispersion again
 rdev <- sum(residuals(modelBQ)^2)
 mdf <- length(fixef(modelBQ))
 rdf <- nrow(numger)-mdf
@@ -136,19 +143,18 @@ Anova(modelBQ, type=3, test="Chisq")
 summary(modelBQ)
 r.squaredGLMM(modelBQ)
 
-#Reducimos el modelo:
-#Selección de la parte fija:
+#Reduce the model:
+#Select the fixed part:
 
-m6<- update(m4, REML =FALSE)#lo primero que tenemos que hacer es ajustar el modelo con la parte aleatorio óptima a ML. 
+m6<- update(m4, REML =FALSE) #The first thing we have to do is to fit the model with the optimal random part to ML. 
 options(na.action = "na.fail")
-sg<- dredge(modelBQ, rank="AICc", fixed = ~Population|Tank) #Evaluamos todos los posibles modelos, con todas las posibles combinaciones de factores fijos, fijando el factor aleatorio. 
+sg<- dredge(modelBQ, rank="AICc", fixed = ~Population|Tank) #We evaluate all possible models, with all possible combinations of fixed factors, fixing the random factor. 
 subset(sg, delta < 2, recalc.weights = TRUE)
 importance(subset(model.sel(sg), delta <= 2))
 
 Muminresults<- write.csv(sg, "MuMIn Preliminary results SurvivalGermlings_(A-M).csv")
 
-#ahora hacemos el analisis post-hoc para la interaccion Population x Emersion
-library(emmeans)
+# Now we do the POST-HOC analysis for the Population x Emersion interaction.
 
 lsm1 <- emmeans(modelBQ, pairwise ~ Population:Emersion, adjust = "Tukey")
 summary(lsm1, type = "response")
@@ -157,7 +163,7 @@ CLD(lsm1, Letters=letters, alpha = 0.95, which = 1)
 
 plot(lsm1)
 
-# Para la grafica
+# To the plot
 numger <- read.table("clipboard", header = TRUE, sep="\t")
 
 ggplot(initialger, aes(x= Population, y= Mean)) + geom_bar(position = "dodge", stat = "identity", width = 0.5, colour ="Black") + 
@@ -170,11 +176,11 @@ p <- ggplot(initialger, aes(x= Population, y= Mean)) +
   geom_text(aes(label = Letters, y = Mean),hjust = "center", vjust = -2, fontface = "bold") +
   scale_y_continuous(expand = c(0.0,0.0),limits = c(0.0, 100), breaks = seq(0.0,100, by = 20)) + 
   xlab("") + ylab("Initial number of germlings/disc") +
-  theme_bw() + #quitamos el fondo gris 
+  theme_bw() + # to remove background
   theme(panel.border= element_blank(),
-        panel.grid.major = element_blank(), # para quitar las lineas internas del plot
+        panel.grid.major = element_blank(), # to remove plot grid
         panel.grid.minor = element_blank(), 
-        axis.line = element_line(colour = "black", size = 0.20), #para ver las lineas a lo largo de los ejes, size= 1, el grosor de los ejes
+        axis.line = element_line(colour = "black", size = 0.20), # to see the lines along the axes, size= 1, the thickness of the axes
         axis.text.x = element_text(face="plain", color = "black", 
                                    size=10, angle = 0), 
         axis.text.y = element_text(face = "plain", color = "black", 
@@ -182,14 +188,14 @@ p <- ggplot(initialger, aes(x= Population, y= Mean)) +
 
 p
 
-#para guardar las figuras en pdf:
-pdf("myplotArosa.pdf",p, width = 4, height = 4) # las medidas vienen por defecto en inches, para converstirlo a cm habrá que hacerlo manualmente
+# Save figures as pdf
+pdf("myplotArosa.pdf",p, width = 4, height = 4) # las medidas vienen por defecto en inches, para converstirlo a cm habrï¿½ que hacerlo manualmente
 
-#Para una grafica de 8x8 cm 
+# To get figure 8x8 cm 
 pdf("myplotArosa.pdf", p,  width = 8/2.54, height = 8/2.54)
 dev.off()
 
-#???otra forma de hacerlo:
+# Another to save the figure
 ggsave("InitialGermlingM.png", p,  width = 8, height = 8, units = "cm", dpi = 600)
 ggsave("InitialGermlingM.pdf", p,  width = 8, height = 8, units = "cm", dpi = 600)
 
